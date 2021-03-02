2dcd139f91116cf2f231848f125ae30716dee810
osher cohen <oscohen@localhost.localdomain>
2021-03-02 22:15:24 +0200

    fix regex

--- discovery-infra/log_scrap.py
+++ discovery-infra/log_scrap.py
@@ -165,7 +165,7 @@ def get_no_name_message(event_message: str, event_names: list):
     for name in event_names:
         event_message = event_message.replace(name, "Name")
     event_message = re.sub(UUID_REGEX, "UUID", event_message)
-    event_message = re.sub(r"^Host .*:", "Host Name:", event_message)
+    event_message = re.sub(r"^Host \S+:", "Host Name:", event_message)
     return event_message
 
 def get_cluster_object_names(cluster_bash_data):
